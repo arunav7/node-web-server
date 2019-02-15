b26f8ee67f4b63439d275be91f4a4387cc0e5953
arunav7 <harsharunav9@gmail.com>
2019-02-16 00:18:53 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+app.set('view engine', 'hbs');
+// app.use is a middleware which tweaks thw working of an express i.e we can host our own html pages
+
+
+hbs.registerPartials(__dirname + '/views/partials');
+hbs.registerHelper('currentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
+app.use((req,res,next) => {
+    var now = new Date().toDateString();
+    var log = `${now} ,${req.method} ,${req.url}`;
+
+    console.log(log);
+    fs.appendFile('server.log', log  + '\n', (err) => {
+        if(err) {
+            console.log(err);
+        }
+    });
+    next();
+});
+
+app.use((req, res, next) => {
+    res.render('maintainence.hbs');
+});
+
+app.use(express.static(__dirname + '/public'));  // this is addedd below the maintainenece page because node will run code sequentially,due to this static page willbe rendered as it was but not according to maintainence.hbs
+
+app.get('/',(req, res) => {
+    //res.send('<h2>Hey Express is Sending Data</h2>');
+    res.render('home.hbs',{
+        welcomeMessage: 'My Home Page',
+        pageTitle: 'Home Page',
+        someText: 'some text here....'
+    });
+});
+
+// when object is sent through response to the page then express automatically parse it into json format
+
+// this will create an another route to localhost:3010/about page/url
+app.get('/about',(req, res) => {
+    res.render('about.hbs',{
+        welcomeMessage: 'Welcome to About Page',
+        pageTitle: 'About Page',
+        someText: 'some text here....'
+    });
+});
+
+app.get('/bad',(req, res) => {
+    res.send({
+        error: 'Sorry something gone wrong...'
+    });
+});
+
+app.listen(3010, () => {
+    console.log('Server is up on port 3010');
+});
